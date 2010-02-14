9e36031bd2a5d6d6170cc3556afaf4f960151ae7
dota.keys <dota.keys@0d7424f6-15d1-11df-9d17-5fea08ab653e>
2010-02-14 20:11:06 +0000

    - added API for UserService and DocumentService - added DTOs for Document - added some exceptions - added Word entity used to represent the wordlist for fulltext search

--- src/java/cz/muni/fi/pa165/docserver/service/UserService.java
+++ src/java/cz/muni/fi/pa165/docserver/service/UserService.java
@@ -5,10 +5,50 @@
 package cz.muni.fi.pa165.docserver.service;
 
 import cz.muni.fi.pa165.docserver.entities.User;
+import cz.muni.fi.pa165.docserver.exceptions.UserCannotBeCreatedException;
 
 /**
  *
  * @author Matus
  */
 public interface UserService extends GenericEntityService<User> {
+
+    /**
+     * Return instance of the {@code User}. Search by ID.
+     *
+     * @param id Unique ID of the user
+     * @return User with the given ID, or null if user not found
+     */
+    User getUserById(long id);
+
+    /**
+     * Return instance of the {@code User}. Search by Name. Each
+     * user has unique name.
+     *
+     * @param name Unique name of the user
+     * @return User with the given name, or null if user not found
+     */
+    User getUserByName(String name);
+
+    /**
+     * Create new {@code User} and reflect changes into persistence media.
+     *
+     * @param name Name of new user
+     * @param password Password of new user
+     * @return Instance of the user just created
+     * @throws UserCannotBeCreatedException
+     *         If user with {@code name} already exists or if there was an error
+     *         during persisting the instance.
+     */
+    User addUser(String name, String password) throws UserCannotBeCreatedException;
+
+    /**
+     * Change password of the user.
+     *
+     * @param id Id of the user whom we want to change the password
+     * @param oldPassword Old password for verification
+     * @param newPassword New password
+     * @return true if password is changed, false otherwise
+     */
+    boolean changePassword(long id, String oldPassword, String newPassword);
 }
