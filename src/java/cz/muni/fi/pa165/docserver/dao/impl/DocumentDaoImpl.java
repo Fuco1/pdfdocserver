f6d81738f741ea66789dda9ce0e5d78d4664fe98
igsneg <igsneg@0d7424f6-15d1-11df-9d17-5fea08ab653e>
2010-02-19 14:21:40 +0000

    libraries:mby

--- src/java/cz/muni/fi/pa165/docserver/dao/impl/DocumentDaoImpl.java
+++ src/java/cz/muni/fi/pa165/docserver/dao/impl/DocumentDaoImpl.java
@@ -6,6 +6,9 @@ package cz.muni.fi.pa165.docserver.dao.impl;
 
 import cz.muni.fi.pa165.docserver.dao.DocumentDao;
 import cz.muni.fi.pa165.docserver.entities.Document;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -14,4 +17,19 @@ import org.springframework.stereotype.Repository;
  */
 @Repository("documentDao")
 public class DocumentDaoImpl extends GenericDaoImpl<Document> implements DocumentDao {
+
+    public List<Document> findByTags(String[] tags) {
+        EntityManager em = getJpaTemplate().getEntityManagerFactory().createEntityManager();
+        StringBuilder sb = new StringBuilder();
+        sb.append("(");
+        for(int i = 0;i<tags.length;i++) {
+            sb.append(" " + tags[i]);
+            if (i!=tags.length) sb.append(",");
+        }
+        sb.append(")");
+        String query = "SELECT d.* FROM (Document d INNER JOIN document_tag it ON d.id = it.document_id) INNER JOIN Tag t ON it.tags_id = t.id WHERE (tag in " + sb.toString() + ") GROUP BY d.id";
+        Query q = em.createNativeQuery(query, Document.class);
+        List<Document> list = (List<Document>) q.getResultList();
+        return list;
+    }
 }
